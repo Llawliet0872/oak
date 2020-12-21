cefde91e2778b29b9b42ba5292a3b38b8b61bf91
Llawliet0872 <arnav0872@gmail.com>
2020-12-21 10:58:42 +0530

    Installer for oak

--- install.cpp
+++ install.cpp
@@ -4,7 +4,7 @@ int main()
 {
     std::string option;
     std::cout << "Installing Dependencies for oak..." << std::endl;
-    system("sudo apt install wget g++ libcurl4-openssl-dev dpkg tar");
+    system("sudo apt install wget grep g++ libcurl4-openssl-dev dpkg tar");
     system("g++ src/oak.cpp src/utils.cpp -o oak -lcurl");
     std::cout << "Would you like to place oak in /bin for global access? [y/n]: ";
     std::cin >> option;
